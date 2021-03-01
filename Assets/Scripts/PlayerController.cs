c82c6bd5988c96d2f905ff3cd50b2fdc4cb12e47
GranasLite <79870977+GranasLite@users.noreply.github.com>
2021-03-01 14:36:22 -0500

    Added Unity project
    Throw beers at animals.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 15.0f;
+    public float xRange = 20.0f;
+    public GameObject projectilePrefab;
+    public Animator death;
+    public bool gameOver;
+    public AudioSource rave;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+       
+        if (!gameOver)
+        {
+            horizontalInput = Input.GetAxis("Horizontal");
+            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //Launch a projectile from the player
+                Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            }
+        }
+
+
+    }
+    public void Die()
+    {
+        death.SetTrigger("Death_b");
+        gameOver = true;
+        rave.Stop();
+    }
+}
